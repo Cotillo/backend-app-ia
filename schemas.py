80bd6770711a6e6bf5b6b6aa96525ebf3a79cb27
Cotillo <122700982+Cotillo@users.noreply.github.com>
2025-04-28 23:11:48 -0300

    Delete schemas.py

--- schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-
-class VariableSuggestion(BaseModel):
-    nombre: str
-    tipo_sugerido: str
-
-class ConfirmVariablesResponse(BaseModel):
-    variables: List[VariableSuggestion]
