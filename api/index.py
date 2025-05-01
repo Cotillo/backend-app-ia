891e24da4452945c17dd3ecb5815fdea7580a769
Cotillo <122700982+Cotillo@users.noreply.github.com>
2025-05-01 12:26:18 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -9,9 +9,20 @@ app = FastAPI()
 
 # Inicializar Firebase solo una vez
 if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase-key.json")  # Asegurate de tener este archivo en la raíz del proyecto
+    cred = credentials.Certificate({
+        "type": os.getenv("FIREBASE_TYPE"),
+        "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+        "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+        "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+        "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+        "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+        "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+        "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+        "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+        "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+    })
     firebase_admin.initialize_app(cred, {
-        'storageBucket': 'dat-in-69b46.appspot.com'  # Cambia esto por el bucket de tu Firebase
+        'storageBucket': os.getenv("FIREBASE_STORAGE_BUCKET")
     })
 
 @app.get("/")
