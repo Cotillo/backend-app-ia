5bd254100b6ff8a4ff72bd9aaf2dda5ebdb17576
Cotillo <122700982+Cotillo@users.noreply.github.com>
2025-05-01 16:11:54 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -9,22 +9,24 @@ app = FastAPI()
 
 # Inicializar Firebase solo una vez
 if not firebase_admin._apps:
-    cred = credentials.Certificate({
-        "type": os.getenv("FIREBASE_TYPE"),
-        "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-        "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-        private_key_env = os.getenv("FIREBASE_PRIVATE_KEY")
-        if private_key_env is None:
-            raise ValueError("FIREBASE_PRIVATE_KEY no está definida en las variables de entorno")
+    private_key_env = os.getenv("FIREBASE_PRIVATE_KEY")
 
-        "private_key": private_key_env.replace("\\n", "\n"),
-        "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-        "client_id": os.getenv("FIREBASE_CLIENT_ID"),
-        "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
-        "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
-        "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
-        "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
-    })
+if private_key_env is None:
+    raise ValueError("FIREBASE_PRIVATE_KEY no está definida en las variables de entorno")
+
+cred = credentials.Certificate({
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": private_key_env.replace("\\n", "\n"),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+})
+
     firebase_admin.initialize_app(cred, {
         'storageBucket': os.getenv("FIREBASE_STORAGE_BUCKET")
     })
